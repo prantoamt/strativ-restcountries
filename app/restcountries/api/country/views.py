74d2ae8b4b42651bcde75a47ff813ad4a5ae47f7
prantoamt <prantoamt@gmail.com>
2021-06-23 00:01:01 +0600

    create method added for countries end-point

--- app/restcountries/api/country/views.py
+++ app/restcountries/api/country/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import viewsets, status
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.serializers import Serializer
 from .serializers import CountrySerializer
 from country.models import Country
 from api.utils.content_object import Content
@@ -28,3 +29,10 @@ class CountryViewSet(viewsets.ModelViewSet):
         content.set_country_details(**{'id': uuid})
         data = content.get_data()
         return Response(data, status=status.HTTP_200_OK)
+
+    def create(self, request, *args, **kwargs):
+        data = request.data.copy()
+        serializer = self.get_serializer(data=data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
