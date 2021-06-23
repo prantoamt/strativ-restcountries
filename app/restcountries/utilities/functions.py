b5783ee2e2707e6dbc1d7d5769cfe645a90641b2
prantoamt <prantoamt@gmail.com>
2021-06-23 21:02:31 +0600

    bug fixed in get_data func

--- app/restcountries/utilities/functions.py
+++ app/restcountries/utilities/functions.py
@@ -7,7 +7,7 @@ def create_and_map_language(country: object, languages: list) -> None:
     Steps of operations:
         1. The function pops and takes the last item from the languages list.
         2. Creates the popped Language's entry in Language table if Language with this name does not exist.
-        3. Maps the country with the newly created language by creating a new entry in CountryLanguage Table.
+        3. Maps the country with the newly created or existing language by creating a new entry in CountryLanguage Table.
         4. Invokes itself recursively until the languages list becomes empty.'''
     if len(languages) == 0:
         return
@@ -15,25 +15,23 @@ def create_and_map_language(country: object, languages: list) -> None:
         language = languages.pop()
         language_obj, created = Language.objects.get_or_create(
             name=language.get('name'))
-        if created:
-            CountryLanguage.objects.get_or_create(
-                country_id=country, language_id=language_obj)
+        CountryLanguage.objects.get_or_create(
+            country_id=country, language_id=language_obj)
         create_and_map_language(country, languages)
 
 
 def create_and_map_neighbouring_country(data: list, country: object, neighbours: list) -> list:
     '''Takes data(list of dicts or list of JSON objects of countries), Country object and a list of its neighbours(alpha3Codes). 
-    After execution, returns the updated data(list of dicts or list of JSON objects of countries)
+    After execution, returns the data(list of dicts or list of JSON objects of countries)
     Steps of operations:
         1. The function pops and takes the last item from neighbours list.
         2. Iterates through the data until an alpha3Code of a country is matched with the popped item.
         3. Once matched, creates a new country(neighbour country) in Country Table if does not exist.
-        4. Creates new Languages which are spoken by the newly created Country.
+        4. Creates and maps Languages which are spoken by the newly created or existing Country.
         5. Maps this country(neighbour country) with the given country by creating new entry in CountryNeighbour Table.
-        6. Removes the country which is newly created at step 3 from the data list so that unneccessary iterations can be inhibited.
-        7. Invokes iteself recursively until the neighbours list becomes empty. During recursion, it sends the
+        6. Invokes iteself recursively until the neighbours list becomes empty. During recursion, it sends the
             updated data list in the argument.
-        8. Finally returns the updated data list.'''
+        7. Finally returns the data list.'''
     if len(neighbours) == 0:
         return data
     else:
@@ -42,14 +40,13 @@ def create_and_map_neighbouring_country(data: list, country: object, neighbours:
             if item.get('alpha3Code', 'None') == neighbour:
                 neighbour_country, created = Country.objects.get_or_create(name=item.get('name'), alphacode_2=item.get('alpha2Code'), capital=item.get(
                     'capital'), population=item.get('population'), timezone=item.get('timezones')[0], flag=item.get('flag'))
-                if created:
-                    create_and_map_language(
-                        neighbour_country, item.get('languages', []))
 
-                    CountryNeighbour.objects.get_or_create(
-                        country_id=country, neighbour_country_id=neighbour_country)
-                    data.remove(item)
-                    break
+                create_and_map_language(
+                    neighbour_country, item.get('languages', []))
+
+                CountryNeighbour.objects.get_or_create(
+                    country_id=country, neighbour_country_id=neighbour_country)
+                break
     return create_and_map_neighbouring_country(data, country, neighbours)
 
 
@@ -57,7 +54,7 @@ def get_data() -> None:
     '''Steps of operations:
         1. The function fetches all countries from the url.
         2. Iterate through each country and creates new entry in Country table.
-        3. Creates Languages that are spoken by the newly created country.
+        3. Creates Languages that are spoken by the country.
         4. Creates and maps the neighbouring countries with the newly created countries.'''
     url = 'https://restcountries.eu/rest/v2/all'
     response = requests.get(url)
@@ -65,7 +62,6 @@ def get_data() -> None:
     for item in data:
         country, created = Country.objects.get_or_create(name=item.get('name'), alphacode_2=item.get('alpha2Code'), capital=item.get(
             'capital'), population=item.get('population'), timezone=item.get('timezones')[0], flag=item.get('flag'))
-        if created:
-            create_and_map_language(country, item.get('languages', []))
-            data = create_and_map_neighbouring_country(
-                data, country, item.get('borders', []))
+        create_and_map_language(country, item.get('languages', []))
+        data = create_and_map_neighbouring_country(
+            data, country, item.get('borders', []))
