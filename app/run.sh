57ecf98d3c412bb126ceab713613130d299df5d2
prantoamt <prantoamt@gmail.com>
2021-06-25 16:36:41 +0600

    port changed

--- app/run.sh
+++ app/run.sh
@@ -2,4 +2,4 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input
-python manage.py runserver 0.0.0.0:8080
+python manage.py runserver 0.0.0.0:8000
